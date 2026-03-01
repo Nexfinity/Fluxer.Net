72dd25e50265c4e3088b93f1475132b89981e090
Builderb <builderbgamer@gmail.com>
2026-03-01 22:48:18 +0000

    Initial fluxer client

---
+++ Fluxer.Net/FluxerClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Fluxer.Net;
+
+public class FluxerClient
+{
+    public FluxerClient(string token, FluxerConfig? config = null)
+    {
+        Token = token;
+        if (config == null)
+            config = new FluxerConfig();
+
+        Rest = new ApiClient(token, config);
+        WebSocket = new GatewayClient(token, config);
+    }
+
+    public string Token { get; }
+
+    public ApiClient Rest { get; }
+
+    public GatewayClient WebSocket { get; }
+
+    public Task StartAsync => WebSocket.ConnectAsync();
+}
