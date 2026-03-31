4bb97abb750c51efb6002c4c1a34e4c5e2cfdf72
Builderb <builderbgamer@gmail.com>
2026-03-31 03:49:10 +0100

    Fix guild helper

--- Fluxer.Net/Rest/Helpers/GuildHelpers.cs
+++ Fluxer.Net/Rest/Helpers/GuildHelpers.cs
@@ -24,7 +24,7 @@ public static class GuildHelpers
         => guild.Client.Rest.GetChannelsAsync(guild.Id);
 
     public static Task<Channel> CreateChannelAsync(this Guild guild, CreateGuildChannelRequest request)
-        => guild.Client.Rest.CreateGuildChannelAsync<ChannelJson>(guild.Id, request);
+        => guild.Client.Rest.CreateGuildChannelAsync(guild.Id, request);
 
     public static Task<IEnumerable<GuildEmoji>> GetEmojisAsync(this Guild guild)
         => guild.Client.Rest.GetEmojisAsync(guild.Id);
