f98980c6b1e6b5507046c14b942bbe4c0b17b133
Float <35084388+Float-as@users.noreply.github.com>
2026-07-28 00:26:10 +0300

    Add groundwork to provide query params to HTTP requests in Rest client

---
+++ Fluxer.Net/Rest/QueryParams.cs
@@ -0,0 +1,10 @@
+﻿namespace Fluxer.Net.Rest;
+
+/// <summary>
+/// Query params for HTTP requests
+/// </summary>
+public static class QueryParams
+{
+    public const string Limit = "limit";
+    public const string After = "after";
+}
